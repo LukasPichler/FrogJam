909e21131172b9a7858537b023fce78f29ffa60f
LauraMarsoner <laura.marsoner@hotmail.com>
2022-05-13 21:39:03 +0200

    leaderboard

--- Assets/Scripts/LeaderboardController.cs
+++ Assets/Scripts/LeaderboardController.cs
@@ -7,14 +7,42 @@ public class LeaderboardController : MonoBehaviour
 {
     public InputField MemberID, PlayerScore;
     public int ID;
-
+    private int MaxScores = 13;
+    public Text[] Entries;
     private void Start()
     {
-        LootLockerSDKManager.StartSession("Player", (response) =>
+        LootLockerSDKManager.StartGuestSession((response) =>
+        {
+            if (!response.success)
+            {
+                Debug.Log("error starting LootLocker session"+ response.ToString());
+
+                return;
+            }
+
+            Debug.Log("successfully started LootLocker session");
+        });
+    }
+
+    public void ShowScores()
+    {
+        LootLockerSDKManager.GetScoreList(ID, MaxScores, (response) =>
         {
             if (response.success)
             {
-                Debug.Log("Success");
+                LootLockerLeaderboardMember[] scores = response.items;
+                for (int i = 0; i < scores.Length; i++)
+                {
+                    Entries[i].text = (scores[i].rank + " " + scores[i].member_id + ".   " + scores[i].score);
+                }
+
+                if (scores.Length < MaxScores)
+                {
+                    for (int i = scores.Length; i < MaxScores; i++)
+                    {
+                        Entries[i].text = (i + 1).ToString() + ".   none";
+                    }
+                }
             }
             else
             {
@@ -22,7 +50,6 @@ public class LeaderboardController : MonoBehaviour
             }
         });
     }
-
     public void SubmitScore()
     {
         LootLockerSDKManager.SubmitScore(MemberID.text, int.Parse(PlayerScore.text), ID, (response) =>
